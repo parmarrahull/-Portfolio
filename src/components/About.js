e8879369167c782ffcd251f6d740d055a3aa44df
Parmar Rahul <parmarrahul1974@gmail.com>
2021-12-15 12:50:10 +0530

    About file changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import SocialContact from './SocialContact';
+import Typed from 'typed.js'
 
 function About() {
+    const el = useRef(null);
+    const typed = useRef(null);
+    useEffect(() => {
+      const options = {
+        strings: [
+          '&nbsp;Rahul Parmar :)',
+          '&nbsp;a <span class="text-red-500">MERN Stack Developer</span>',
+        //   '&nbsp;a Freelancer',
+        ],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true,
+        showCursor: true,
+      };
+      typed.current = new Typed(el.current, options);
+  
+      return () => {
+        typed.current.destroy();
+      };
+    }, []);
     return (
         <div className="about">
             <div className="about__top">
                 <div className="about__info">
                     Hello There 👋, I am
-                    <br /> <span className="info__name">Rahul Parmar</span>
+                    <span ref={el} />
+                    {/* <br /> <span className="info__name">Rahul Parmar</span>
                     <br /> React-JS Developer 👨‍💻
-                    <br />I love experimenting with the web.
+                    <br />I love experimenting with the web. */}
                 </div>
                 <div className="about__photo">
                     <img src={require("../img/Coding.png").default} alt="imgge" className="picture" />
